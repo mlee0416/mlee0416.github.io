efbd16b53bd66acf73de2fc652440741706204ab
Michael Lee <mlee0416@gmail.com>
2024-07-11 12:33:07 -0700

    add version

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,11 @@
 import LoginForm from "@/components/auth/LoginForm";
 import React from "react";
-
+import packageJSON from "@/../package.json";
 const Login = () => {
   return (
-    <div>
+    <div className="space-y-4">
       <LoginForm />
+      <p className="w-full flex justify-end">v.{packageJSON.version}</p>
     </div>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -9,7 +8,7 @@ export const metadata = {
     default: "Personal App",
     template: "%s | Next.js 14",
   },
-  description: "Next.js starter app description",
+  description: "Personal App",
 };
 
 export default function RootLayout({ children }: any) {
